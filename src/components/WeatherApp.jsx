c21016faf0f70dc480f5e8aaa7de1220e02ca58b
luisangeluis <luisangeluis@gmail.com>
2022-06-03 16:53:18 -0500

    1 file modified

--- src/components/WeatherApp.jsx
+++ src/components/WeatherApp.jsx
@@ -6,38 +6,34 @@ const WeatherApp = () => {
 
   useEffect(() => {
     navigator.geolocation.getCurrentPosition(success);
-    getApi(coord);
   }, []);
 
-  let coord = {
-    latitude: 1,
-    longitude: 1,
-  };
-
   function success(crd) {
-    console.log(crd);
-    coord.latitude = crd.coords.latitude;
-    coord.longitude = crd.coords.latitude;
-    console.log(crd.coords.latitude);
-    console.log(crd.coords.latitude);
-  }
+    let latitude = crd.coords.latitude;
+    let longitude = crd.coords.latitude;
 
-  const getApi = (coord) => {
-    const key = '4214c6fe0c0be71f13084263dd5761b1';
-    axios
-      .get(
-        `https://api.openweathermap.org/data/2.5/weather?lat={${coord.latitude}}&lon={${coord.longitude}}&appid={${key}}`
-      )
-      .then((res) => console.log(res))
-      .catch((error) => console.log(error));
-  };
+    const getApi = () => {
+      const key = '4214c6fe0c0be71f13084263dd5761b1';
+      axios
+        .get(
+          `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&exclude=hourly&appid=${key}`
+        )
+        .then((res) => {
+          console.log(res.data);
+          setWeather(res.data);
+        })
+        .catch((error) => console.log(error));
+    };
+
+    getApi();
+  }
 
   return (
     <>
       <div className="card">
-        <img src="..." className="card-img-top" alt="..." />
+        {/* <img src="..." className="card-img-top" alt="..." /> */}
         <div className="card-body">
-          <h5 className="card-title">Card title</h5>
+          <h5 className="card-title">{weather?.name}</h5>
           <p className="card-text">
             Some quick example text to build on the card title and make up the
             bulk of the card's content.
