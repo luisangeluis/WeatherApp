7df7c2021d94446cdf2c3803ca8a9983ba52b0a1
luisangeluis <luisangeluis@gmail.com>
2022-06-06 14:18:35 -0500

    1 binaries uploaded

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react';
 import './App.css';
 import WeatherApp from './components/WeatherApp';
+import log from './logo.svg';
+import clima from './clima.png';
 
 function App() {
   const [count, setCount] = useState(0);
   return (
-    <div className="App">
+    <div className="App" style={{ backgroundImage: `url(${clima})` }}>
       <div className="weather-app">
         <WeatherApp />
       </div>
