308216042246a280405df8bff5708532da75ba74
luisangeluis <luisangeluis@gmail.com>
2022-06-06 13:27:13 -0500

    1 file modified

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
     <div
       className="App"
       style={{
-        backgroundImage: 'url("' + clima + '")',
+        backgroundImage: `url(${clima})`,
         // backgroundColor: 'red',
       }}
     >
