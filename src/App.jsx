71ee59eaf751db5f7cb005f94e935f2d65646284
luisangeluis <luisangeluis@gmail.com>
2022-06-06 23:01:42 -0500

    Dise;o y background.

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import { useState } from 'react';
 import './App.css';
 import WeatherApp from './components/WeatherApp';
-import log from './logo.svg';
-import clima from './clima.png';
 
 function App() {
   const [count, setCount] = useState(0);
   return (
-    <div className="App" style={{ backgroundImage: `url(${clima})` }}>
+    <div className="App">
       <div className="weather-app">
         <WeatherApp />
       </div>
--- src/components/WeatherApp.jsx
+++ src/components/WeatherApp.jsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import WeatherBtn from './WeatherBtn';
-import clima from '../clima.png';
 
 const WeatherApp = () => {
   const [coord, setCoord] = useState();
@@ -50,54 +49,51 @@ const WeatherApp = () => {
   // console.log(weather);
 
   return (
-    <div
-      className="container wheather-app_container"
-      style={{ backgroundImage: `url(${clima})` }}
-    >
-      {!weather ? (
+    <div className="container wheather-app_container">
+      {weather ? (
+        <div className="card">
+        <div className="card-body">
+          <h2>Weather App</h2>
+          <h2 className="card-title p-2">
+            {weather?.name}, {weather?.sys.country}
+          </h2>
+          <div className="row">
+            <div className="col-sm-5 p-2">
+              <img
+                src={`http://openweathermap.org/img/wn/${weather?.weather[0].icon}@4x.png`}
+                alt=""
+                className="img-fluid"
+              />
+              <h3>
+                {fahrenheit
+                  ? `${convert().toFixed(2)} °F`
+                  : `${weather?.main.temp} °C`}
+              </h3>
+            </div>
+            <div className="col-sm-7 text-center text-sm-start p-2">
+              <h3 className="text-center">
+                "{weather?.weather[0].description}"
+              </h3>
+              <h3>
+                <i className="fa-solid fa-wind"></i>Wind speed{' '}
+                <b>{weather?.wind.speed} m/s</b>
+              </h3>
+              <h3>
+                <i className="fa-solid fa-cloud"></i>Clouds{' '}
+                <b>{weather?.clouds.all}%</b>
+              </h3>
+              <h3>
+                <i className="fa-solid fa-temperature-half"></i>Pressure{' '}
+                <b>{weather?.main.pressure}mb</b>
+              </h3>
+            </div>
+          </div>
+          <WeatherBtn convertDegrees={convertDegrees} />
+        </div>
+      </div>
+      ) : (
         <div className="spinner-border" role="status">
           <span className="visually-hidden">Loading...</span>
-        </div>
-      ) : (
-        <div className="card">
-          <div className="card-body">
-            <h2>Weather App</h2>
-            <h2 className="card-title p-2">
-              {weather?.name}, {weather?.sys.country}
-            </h2>
-            <div className="row">
-              <div className="col-sm-5 p-2">
-                <img
-                  src={`http://openweathermap.org/img/wn/${weather?.weather[0].icon}@4x.png`}
-                  alt=""
-                  className="img-fluid"
-                />
-                <h3>
-                  {fahrenheit
-                    ? `${convert().toFixed(2)} °F`
-                    : `${weather?.main.temp} °C`}
-                </h3>
-              </div>
-              <div className="col-sm-7 text-center text-sm-start p-2">
-                <h3 className="text-center">
-                  "{weather?.weather[0].description}"
-                </h3>
-                <h3>
-                  <i className="fa-solid fa-wind"></i>Wind speed{' '}
-                  <b>{weather?.wind.speed} m/s</b>
-                </h3>
-                <h3>
-                  <i className="fa-solid fa-cloud"></i>Clouds{' '}
-                  <b>{weather?.clouds.all}%</b>
-                </h3>
-                <h3>
-                  <i className="fa-solid fa-temperature-half"></i>Pressure{' '}
-                  <b>{weather?.main.pressure}mb</b>
-                </h3>
-              </div>
-            </div>
-            <WeatherBtn convertDegrees={convertDegrees} />
-          </div>
         </div>
       )}
     </div>
